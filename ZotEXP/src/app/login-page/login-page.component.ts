3be2b29562248a69be856a1cc1b30c9a0215b8e6
Kenny Kim <kyungrk.@uci.edu>
2025-05-03 22:00:33 -0700

    Added login and signup

--- ZotEXP/src/app/login-page/login-page.component.ts
+++ ZotEXP/src/app/login-page/login-page.component.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  signupUsers: any [] = [];
+  signupObj:any = {
+    username: '',
+    email: '',
+    password: '',
+    passwordCheck: ''
+  }
+  loginObj:any = {
+    username: '',
+    password: ''
+  };
+  constructor () { }
 
+  ngOnInit(): void {
+    const localData = localStorage.getItem('signUpUsers');
+    if (localData != null){
+      this.signupUsers = JSON.parse(localData);
+    }
+  }
+
+  onSignUp() {
+    this.signupUsers.push(this.signupObj);
+    localStorage.setItem("SignupUsers", JSON.stringify(this.signupUsers));
+    this.signupObj = {
+      username: '',
+      email: '',
+      password: '',
+      passwordCheck: ''
+    }
+  }
+
+  onLogIn() {
+    const isUserExit = this.signupUsers.find(m => m.username == this.loginObj.username && m.password == this.loginObj.password);
+    if (isUserExit != undefined){
+      alert("User Login Successfully"); 
+    } else{
+      alert('Failed to Login');
+    }
+  }
 }
