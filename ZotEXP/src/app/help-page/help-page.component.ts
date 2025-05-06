0900d830908b6af652de2fdb92b78c16976e06cb
Kenny Kim <kyungrk.@uci.edu>
2025-05-06 07:50:10 -0700

    Added help and login pages

--- ZotEXP/src/app/help-page/help-page.component.ts
+++ ZotEXP/src/app/help-page/help-page.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-help-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './help-page.component.html',
   styleUrl: './help-page.component.css'
 })
 export class HelpPageComponent {
-
+  feedback: any [] = []; 
+  feedbackForm:FormGroup;
+  formSubmitted = false;
+  feedbackObj:any = {
+    email: '',
+    type: '',
+    description: ''
+  };
+  constructor(private userInput: FormBuilder) {
+    this.feedbackForm = userInput.group({
+      email: ['', Validators.required],
+      corresponse: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+    
+  }
+  
+  onSubmit(){
+    if (this.feedbackForm.valid){
+      this.formSubmitted = true; 
+      let localData = localStorage.getItem("Feedback"); 
+      this.feedback.push(
+        this.feedbackForm
+      );
+      // default setting 
+      this.feedbackObj = {
+        email: '',
+        type: '',
+        description: ''
+      };
+    }
+  }
 }
--- ZotEXP/src/app/login-page/login-page.component.ts
+++ ZotEXP/src/app/login-page/login-page.component.ts
@@ -7,18 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+  showSignUp: boolean = false; 
   signupUsers: any [] = [];
   signupObj:any = {
     username: '',
     email: '',
     password: '',
     passwordCheck: ''
-  }
+  };
   loginObj:any = {
     username: '',
     password: ''
   };
   constructor () { }
+
+  toggleSignUp() {
+    this.showSignUp = !this.showSignUp;
+  }
 
   ngOnInit(): void {
     const localData = localStorage.getItem('signUpUsers');
@@ -28,14 +33,60 @@ export class LoginPageComponent {
   }
 
   onSignUp() {
-    this.signupUsers.push(this.signupObj);
+    // this.signupUsers.push(this.signupObj);
+    // localStorage.setItem("SignupUsers", JSON.stringify(this.signupUsers));
+    let localData = localStorage.getItem("SignupUsers"); 
+    this.signupUsers = localData ? JSON.parse(localData) : []; 
+
+    if (this.signupObj.password != this.signupObj.passwordCheck){
+      alert("Passwords do not match!");
+      return; 
+    }
+
+    // Username Check 
+    const usernameExists = this.signupUsers.some(user => 
+      user.username == this.signupObj.username
+    );
+    if (usernameExists) {
+      alert("Username already exists!"); 
+      return;
+    }
+
+    // Email Check 
+    const emailExists = this.signupUsers.some(user =>
+      user.email == this.signupObj.email
+    );
+    if (emailExists){
+      alert("Email already exists!"); 
+      return;
+    }
+
+    // Password Check 
+    const passwordExists = this.signupUsers.some(user =>
+      user.password == this.signupObj.password
+    );
+    if (passwordExists){
+      alert("Passwod is already used by other user");
+      return; 
+    }
+
+    // Add new account 
+    this.signupUsers.push({
+      username: this.signupObj.username,
+      email: this.signupObj.email,
+      password: this.signupObj.password
+    });
     localStorage.setItem("SignupUsers", JSON.stringify(this.signupUsers));
+
     this.signupObj = {
       username: '',
       email: '',
       password: '',
       passwordCheck: ''
     }
+
+    this.toggleSignUp(); 
+    alert("Account Created Successfully");
   }
 
   onLogIn() {
