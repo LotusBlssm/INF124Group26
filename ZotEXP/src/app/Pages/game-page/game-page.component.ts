fad92e17156abda0c770f5738fac74f5dbcec696
David Overman <portalerd@icloud.com>
2025-06-03 19:07:27 -0700

    Clean game page component

--- ZotEXP/src/app/Pages/game-page/game-page.component.ts
+++ ZotEXP/src/app/Pages/game-page/game-page.component.ts
@@ -3,6 +3,7 @@ import { GameTagComponent } from '../../game-tag/game-tag.component';
 import { UserTagComponent } from '../../user-tag/user-tag.component';
 import { ReviewComponent } from '../../review/review.component';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game-page',
@@ -17,18 +18,7 @@ import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angula
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class GamePageComponent {
-  gameData:any = { // will be instantiated via backend later 
-    title: "Game",
-    image: undefined,
-    company: "Company",
-    releaseDate: new Date(1999, 8, 22),
-    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.",
-    rating: Math.random() * 5,
-    gameTags: ["Multiplayer", "Controller Friendly", "Microtransactions", "Co-Op", "Mod Supported", "Fun with Friends"],
-    userTags: ["Teamwork Heavy", "Not for Noobs", "Mod Supported", "Replayable", "Friendly Community"]
-  }
-  reviews:any = this.getReviews()
-
+  gameData:any;
   // User's review form construction
   userReview: any [] = []; 
   userReviewForm:FormGroup;
@@ -36,37 +26,19 @@ export class GamePageComponent {
   userReviewObj:any = {
     userReviewText: ''
   };
-  constructor(private userInput: FormBuilder) {
-    this.userReviewForm = userInput.group({
-      userReviewText: ['', Validators.required]
-    });
-  }
-  
 
-  // Displayed reviews collection
-  getReviews () { 
-    // will change when backend is made 
-    function randomDate(start: Date, end: Date) {
-      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    }
-    let names = ['Brian Griffin', 'Jane Doe', 'John Doe', 'Stan Marsh', 'Edmond Honda']
-    let reviews = [];
-    for (let i = 0; i < 19; ++i) {
-      reviews.push({
-        date: randomDate(new Date(2000, 0, 1), new Date()),
-        memberName: names[Math.floor((Math.random() * names.length))],
-        profilePicture: undefined,
-        rating: Math.random() * 5,
-        userTags: ["Teamwork Heavy", "Not for Noobs", "Mod Supported"],
-        message: 'Yeah I thought the game was pretty coo, I picked up Sombra and for some reason everyone in my game was mad at me. I now pick Hammond and throw matches for most of my fun, and pick other games to try competitively.'
-      });
-    }
-    console.log(reviews);
-    return reviews;
+  // Necessary to load gameData
+  get id() {
+    return Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+  loadGameData() {
+    // TODO: load game data (from our database, or igdb if needed)
+
   }
 
   onSubmit() {
-    if (this.userReviewForm.valid){
+    if (this.userReviewForm.valid) {
       this.formSubmitted = true; 
       let localData = localStorage.getItem("Feedback"); 
       this.userReview.push(
@@ -77,5 +49,13 @@ export class GamePageComponent {
         userReviewText: ''
       };
     }
+  }
+  
+  constructor(private userInput: FormBuilder, private route: ActivatedRoute) {
+    this.userReviewForm = userInput.group({
+      userReviewText: ['', Validators.required]
+    });
+
+    console.log(this.id);
   }
 }
