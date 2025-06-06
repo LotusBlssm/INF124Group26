b28dc89fc4e55296bfed03e1e2063dfb8d4d639b
Kenny Kim <kyungrk.@uci.edu>
2025-06-05 21:17:27 -0700

    Update DynamoDBs (users and reviews)

--- backend/dynamoClient.js
+++ backend/dynamoClient.js
@@ -1,17 +1,37 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
 
+const secret = "..."; // made up the scret password for jwt
+
 const AWS = require('aws-sdk');
 AWS_ACCESS_KEY_ID='';
 AWS_SECRET_ACCESS_KEY='';
-AWS.config.update({
-  region: 'us-east-1', // or your region
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
 
-const dynamodb = new AWS.DynamoDB.DocumentClient();
-module.exports = dynamodb;
+// AWS.config.update({
+//   region: 'us-east-1', // or your region
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+// });
+
+// Users DynamoDB
+const usersDynamoDB = new AWS.DynamoDBDocumentClient.from(new DynamoDBclient({
+  region: 'us-east-1', // or your region
+  credential: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+}));
+
+// Reviews DynamoDB
+const reviewsDynamoDB = new AWS.DynamoDBDocumentClient.from(new DynamoDBclient({
+  region: 'us-east-1', // or your region
+  credential: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+}));
+
+module.exports = { userDynamoDB, reviewsDynamoDB }; 
 
 const listTables = async () => {
   try {
@@ -21,6 +41,41 @@ const listTables = async () => {
     console.error("Error connecting to DynamoDB:", err.message);
   }
 };
+
+
+
+// SOME WORK RELATED TO DB (login)...
+
+// LOG IN USER
+// const loginUser = async (req, res) => {
+//   const { email, password } = req.body; 
+
+//   AWS.config.update(config.aws_remote_config); 
+//   const docClient = new AWS.DynamoDB.DocumentClient(); 
+
+//   const params = {
+//     TableName: config.aws_table_name, 
+//     Key: { userID } // primary key for Users 
+//   };
+
+//   try {
+//     const data = await docClient.get(params).promise(); 
+//     const user = data.Item; 
+
+//     if (!user) {
+//       return res.status(401).json({ success: false, message: "User not found" });
+//     }
+
+//     const token = jwt.sign({ userID: user.id }, secret, {expiresIn: '1h'})
+
+//     res.josn({ success : true, token }); 
+//   } catch (err) {
+//     console.error("Login error:", err); 
+//     res.status(500).json({ success : false, message : "Internal Server Error" });
+//   }
+// };
+
+
 
 // app.get('/user/:id', async (req, res) => {
 //   const userID = req.params.id; 
