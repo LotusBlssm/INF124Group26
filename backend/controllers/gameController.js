148f12d3c78e31795cfd50292d616d29569fa8ef
reecemalone <reecemalone03@gmail.com>
2025-06-09 11:32:33 -0700

    Fixed getGame() functionality

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -37,8 +37,7 @@ export const getGame = async (req, res) => {
     
     // Prep initial GameTable query
     const id = req.params.id;
-    console.log('id: ' + id);
-    console.log('getGame() in backend controller called. Fetching from database...')
+    console.log('getGame() in backend controller called. Fetching from database game with id', id);
     const params = {
         TableName: 'GameTable',
         Key: { gameID: id }
@@ -55,45 +54,54 @@ export const getGame = async (req, res) => {
         console.log('200 - Retrieved game data', gameData.Item);
         // Now we've got the game, can we grab the review(s) and user(s) associated with it?
 
-        // Make the batch query for all the reviews...
-        const gameReviewData = await getReviews(gameData.Item.reviews, dynamoClient);
-        if (!gameReviewData) {
-            console.log("404 - Couldn't grab reviews");
+        // Initialize the review/user arrays before processing...
+        let gameReviewData = [];
+        let gameReviewUserData = [];
+        if (gameData.Item.reviews.length == 0) {
+            // No, so we'll have our review/user lists just be empty arrays
+            console.log("404 - No reviews to grab");
         } else {
-            console.log("200 - Grabbed reviews ", gameReviewData);
+            // Make the batch query for all the reviews...
+            const gameReviewData = await getReviews(gameData.Item.reviews, dynamoClient);
+            if (!gameReviewData) {
+                console.log("404 - Couldn't grab reviews");
+            } else {
+                console.log("200 - Grabbed reviews ", gameReviewData);
+            }
+            // Now can we get the users corresponding to each review?
+
+            // First we'll need to build a list of userIDs from the reviews...
+            const gameReviewUserIDs = [];
+            for (let i = 0; i < gameReviewData.length; i++) {
+                gameReviewUserIDs[i] = gameReviewData[i]['user_id'];
+            } // There's definitely a cleaner way to do this with .map() or something...
+            // console.log("gameReviewUserIDs: ", gameReviewUserIDs);
+
+            // Then, we can send off our batch query for all these users
+            const gameReviewUsersData = await getUsers(gameReviewUserIDs, dynamoClient);
+            if (!gameReviewUsersData) {
+                console.log("404 - Couldn't grab reviews' users");
+            } else {
+                console.log("200 - Grabbed reviews' users ", gameReviewUsersData);
+            }
         }
-        // Now can we get the users corresponding to each review?
 
-        // First we'll need to build a list of userIDs from the reviews...
-        const gameReviewUserIDs = [];
-        for (let i = 0; i < gameReviewData.length; i++) {
-            gameReviewUserIDs[i] = gameReviewData[i]['user_id'];
-        } // There's definitely a cleaner way to do this ^ with .map()...
-        console.log("gameReviewUserIDs: ", gameReviewUserIDs);
-
-        // Then, we can send off our batch query for all these users
-        const gameReviewUsersData = await getUsers(gameReviewUserIDs, dynamoClient);
-        if (!gameReviewUsersData) {
-            console.log("404 - Couldn't grab reviews' users");
-        } else {
-            console.log("200 - Grabbed reviews' users ", gameReviewUsersData);
+        // Generate & fill the resulting Game object
+        let gameReturn = {
+            id : id,
+            title : gameData.Item.title,
+            imageURL : gameData.Item.imageURL,
+            company : gameData.Item.company,
+            releaseDate : new Date(gameData.Item.releaseDate),
+            description : gameData.Item.description,
+            rating : gameData.Item.rating,
+            gameTags : gameData.Item.gameTags,
+            userTags : gameData.Item.userTags,
+            reviews : gameReviewData
         }
+        // Now we've got our final return object, let's send it back
+        res.json(gameReturn);
 
-            // // Generate & fill the resulting Game object
-            // gameReturn = new Game();
-            // gameReturn.id = id;
-            // // gameReturn.title = gameData.Item.title;
-            // // gameReturn.imageURL = gameData.Item.imageURL;
-            // // gameReturn.company = gameData.Item.company;
-            // gameReturn.releaseDate = new Date();
-            // // gameReturn.description = gameData.Item.description;
-            // // gameReturn.rating = gameData.Item.rating;
-            // // gameReturn.gameTags = gameCompanyTags
-            // // gameReturn.userTags = gameUserTags
-            // gameReturn.reviews = gameReviewData;
-
-            // TODO: UNFINISHED, should build game object (but how?)
-            res.json(gameData);
     } catch (err) {
         console.error('500 - DynamoDB error:', err);
         res.status(500).json({ error: 'Could not retrieve game' });
